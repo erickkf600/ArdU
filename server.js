28f2901b9224a3ba9a45fb8c259a1f82719f7f6d
Erick Ferreira <erickkf600@gmail.com>
2021-02-01 20:42:36 -0300

    fix deploy

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express')
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
 
-const app = express()
-const { } = require('path')
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
-app.use('/', 
-    express.static(
-        resolve(
-            __dirname,
-            './build'
-        )
-    )
-)
-
-app.listen(process.env.PORT || 3000, (err)=>{
-    if(err) {return console.log(err)}
-
-    console.log('Sucesso')
-})
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
